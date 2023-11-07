47639e160d84c743dbb0f6fe92d3d203f3016cac
Bartosz <32547608+bartoszlagoda@users.noreply.github.com>
2023-11-07 23:07:10 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const app = express()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.get("/", (_req, res) => {
+  res.status(200).send("Welcome to my Express")
+})
 
-module.exports = router;
+module.exports = app;
